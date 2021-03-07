1cb41d9be19f1b874f68f32e5b0dd47794812261
root <grzegorzchrymecki@php-developer.ie>
2021-03-07 13:40:54 +0000

    all ok

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,12 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import generateHeader from './js/generate-header';
+
 // start the Stimulus application
 import './bootstrap';
+
+// just for test (use different files
+const h1 = document.createElement('h1');
+h1.innerHTML = generateHeader('Name');
+document.getElementById('pagesIndex').appendChild(h1)
---
+++ assets/js/generate-header.js
@@ -0,0 +1,3 @@
+export default function generateHeader(name){
+    return `Welcome ${name}`;
+}
